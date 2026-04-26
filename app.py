82ac0043aadc425e21c345b32771ac702b5b67a2
rachitjais <rachit.jaiswal64@gmail.com>
2026-04-26 19:40:46 +0530

    Added database and User model

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from models import db
 
 app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 @app.route('/')
 
@@ -8,4 +14,6 @@ def home():
     return{"message":"Devtrack Running"}
 
 if __name__=="__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
